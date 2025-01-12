c1e47737c599bda274ffb9989905a8c9854234f4
Your Name <you@example.com>
2025-01-12 10:45:20 +0000

    Add Milestone_4.py with initial functionality

---
+++ Milestone_4.py
@@ -0,0 +1,33 @@
+import random
+
+class Hangman:
+    def __init__(self, word_list, num_lives=5):
+        self.word_list = word_list
+        self.word = random.choice(word_list)
+        self.word_guessed = ['_' for _ in self.word]
+        self.num_letters = len(set(self.word))
+        self.num_lives = num_lives
+        self.list_of_guesses = []
+
+    def check_guess(self, guess):
+        guess = guess.lower()
+        if guess in self.word:
+            print(f"Good guess! {guess} is in the word.")
+
+    def ask_for_input(self):
+        while True:
+            guess = input("Please guess a letter: ")
+            if len(guess) != 1 or not guess.isalpha():
+                print("Invalid letter. Please, enter a single alphabetical character.")
+            elif guess in self.list_of_guesses:
+                print("You already tried that letter!")
+            else:
+                self.check_guess(guess)
+                self.list_of_guesses.append(guess)
+                break
+            
+# Example usage to test Step 3
+if __name__ == "__main__":
+    word_list = ["apple", "banana", "cherry"]
+    game = Hangman(word_list)
+    game.ask_for_input()
